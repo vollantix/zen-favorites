ffb8afa3842ee4d77e01491fac52224e409f1a12
Vollantix <vollantix@gmail.com>
2026-05-13 00:15:50 +0200

    Add minimal addon config

---
+++ addons/main/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches {
+    class zen_filter_main {
+        name = "ZEN Filter";
+        author = "vollantix";
+        requiredVersion = 2.18;
+        requiredAddons[] = {
+            "cba_main",
+            "zen_main"
+        };
+        units[] = {};
+        weapons[] = {};
+    };
+};
